fabe4f94b8fa75c61ceeab06d2d61530782eefff
darenchun <darenchun@naver.com>
2023-08-22 17:35:53 +0900

    ..

--- src/main/java/com/portfolio/diary/config/appConfig/PwEncrypt_Config.java
+++ src/main/java/com/portfolio/diary/config/appConfig/PwEncrypt_Config.java
@@ -3,12 +3,9 @@ package com.portfolio.diary.config.appConfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -23,16 +20,4 @@ public class PwEncrypt_Config {
         log.info("pwEncoder()");
         return new BCryptPasswordEncoder();
     }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
-                        .requestMatchers("/**").hasRole("USER"))
-                .formLogin(Customizer.withDefaults());
-        httpSecurity.csrf(Customizer.withDefaults());
-        httpSecurity.httpBasic(Customizer.withDefaults());
-        return httpSecurity.build();
-    }
-
 }
